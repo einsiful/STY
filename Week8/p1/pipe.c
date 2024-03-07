1f5b010ef2ac476f48196fcabf6e77e947c34f38
Einar Árni Bjarnson <54355067+einsiful@users.noreply.github.com>
2024-03-07 11:14:39 +0000

    sheeeeeessh

--- Week8/p1/pipe.c
+++ Week8/p1/pipe.c
@@ -37,13 +37,9 @@ char* get_output(char *argv[]) {
     else if (child_pid == 0) {
         close(pipefd[0]);
         dup2(pipefd[1], STDOUT_FILENO);
-
-        if (execvp(argv[0], argv) == -1) {
-            perror("execvp failed");
-            exit(255);
-            return NULL;
-        }
-
+        execvp(argv[0], argv);
+        perror("execvp failed");
+        exit(255);
 
 
 
