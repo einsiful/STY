7d9651d176cb23ac34dea794fa1c2f6910b7e6c9
Einar Árni Bjarnson <54355067+einsiful@users.noreply.github.com>
2024-03-08 08:56:01 +0000

    Vonandi virkar test2

--- Week8/p1/pipe.c
+++ Week8/p1/pipe.c
@@ -1,3 +1,5 @@
+// This changes the way some includes behave.
+// This should stay before any include.
 #define _GNU_SOURCE
 
 #include "pipe.h"
@@ -6,68 +8,76 @@
 #include <stdlib.h> /* For exit */
 #include <stdio.h>
 #include <string.h>
+          
+#define buffer_size 1024
 
-#define buf_size 1024
-
-char* get_output(char *argv[]) {
+char *get_output(char *argv[])
+{
 
     int pipefd[2];
-
-    char buffer[buf_size];
-    char *ptr = malloc(buf_size);
-
-    if (ptr == NULL) {
+    char buffer[buffer_size];
+    char *ptr = malloc(buffer_size);
+    if (ptr == NULL)
+    {
         perror("malloc failed");
         return NULL;
     }
 
-    if (pipe(pipefd) == -1) {
+    if (pipe(pipefd) == -1) 
+    {
         perror("pipe failed");
         return NULL;
     }
 
     int child_pid = fork();
-    if (child_pid == -1) {
+    if (child_pid == -1)
+    {
         perror("fork failed");
         close(pipefd[0]);
         close(pipefd[1]);
         return NULL;
-    }
+    } 
     else if (child_pid == 0) 
     {
         dup2(pipefd[1], STDOUT_FILENO);
         close(pipefd[0]);
         close(pipefd[1]);
-        if(execvp(argv[0], argv) == -1){
-                perror("execvp failed");
+
+        if(execvp(argv[0], argv) == -1)
+        {
+	        perror("execvp failed");
             exit(255);
             return NULL;
-        
         }
     }
-    else {
-            int status;
+    else
+    {
+
+	    int status;
         waitpid(child_pid, &status, 0);
-
-        ssize_t bytes_read = read(pipefd[0], buffer, buf_size);
-
+        
+        ssize_t bytes_read = read(pipefd[0], buffer, buffer_size);
+        
         close(pipefd[0]);
         close(pipefd[1]);
-
-        if (bytes_read == -1) {
+        
+        if (bytes_read == -1) 
+        {
             perror("read failed");
             return NULL;
         }
 
-        else{
+
+
+        else 
+        {
             int i;
-            for (i = 0; i < buf_size && buffer[i] != '\0'; i++) {
+            for (i = 0; i < buffer_size && buffer[i] != '\n'; i++) 
+            {
                 ptr[i] = buffer[i];
             }
-            ptr[i] = '\0';
+            ptr[i] = '\0'; // Null-terminate the string
+        }
     }
-    }
-
     return ptr;
-    }
-
+}
