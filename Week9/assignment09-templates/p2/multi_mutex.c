28d13eda1163241a51d77ba7c285f68abb77abc6
Hlynur Isak <isakvillason@hotmail.com>
2024-03-18 21:42:32 +0000

    multi_mutex not tested

--- Week9/assignment09-templates/p2/multi_mutex.c
+++ Week9/assignment09-templates/p2/multi_mutex.c
@@ -5,17 +5,34 @@
 
 #include "multi_mutex.h"
 
-int multi_mutex_unlock(pthread_mutex_t **mutexv, int mutexc)
-{
-    (void) mutexv;
-    (void) mutexc;
-    return 0;
+int multi_mutex_unlock(pthread_mutex_t **mutexv, int mutexc) {
+    if (mutexv == NULL || mutexc <= 0) {
+        return -1;
+    }
+
+    int ret = 0;
+    for (int i = 0; i < mutexc; ++i) {
+        if (pthread_mutex_unlock(mutexv[i]) != 0) {
+            ret = -1;
+        }
+    }
+    return ret;
 }
 
-int multi_mutex_trylock(pthread_mutex_t **mutexv, int mutexc)
-{
-    (void) mutexv;
-    (void) mutexc;
+int multi_mutex_trylock(pthread_mutex_t **mutexv, int mutexc) {
+    if (mutexv == NULL || mutexc <= 0) {
+        return -1;
+    }
+
+    int i;
+    for (i = 0; i < mutexc; ++i) {
+        if (pthread_mutex_trylock(mutexv[i]) != 0) {
+
+            while (--i >= 0) {
+                pthread_mutex_unlock(mutexv[i]);
+            }
+            return -1;
+        }
+    }
     return 0;
 }
-
