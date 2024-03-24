34f9b470357680c27003d809d86c5d39e6b67b56
Einar Árni Bjarnson <54355067+einsiful@users.noreply.github.com>
2024-03-24 12:48:47 +0000

    p2

--- Week10/assignment10-templates/p2/copy.c
+++ Week10/assignment10-templates/p2/copy.c
@@ -50,12 +50,12 @@ int parseCopyArgs(int argc, char * const argv[], CopyArgs* args)
 	return 0;
 }
 
-
 typedef struct {
     char *from;     // Source file path
     char *to;       // Destination file path
     int blocksize;  // Block size for copying
 } CopyArgs;
+
 
 int doCopy(CopyArgs* args) {
     if (args == NULL || args->from == NULL || args->to == NULL || args->blocksize <= 0) {
